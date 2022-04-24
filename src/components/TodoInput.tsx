30c864547dca8f9ae8cd67318a4fb264d80db963
lucaslomba <lucasazevedolomba@gmail.com>
2022-04-24 10:38:58 -0300

    Corrigindo onPress

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -32,7 +32,7 @@ export function TodoInput({ addTask }: TodoInputProps) {
         testID="add-new-task-button"
         activeOpacity={0.7}
         style={styles.addButton}
-        onPress={() => addTask(task)}
+        onPress={handleAddNewTask}
       >
         <Icon name="chevron-right" size={24} color="#B2B2B2" />
       </TouchableOpacity>
